957956e761f5cf0b199450d73cda34f3788665a5
Felipe Brugnera <165804740+lipebrug@users.noreply.github.com>
2024-05-02 22:29:40 -0300

    Update VideoDigital.java

--- VideoDigital.java
+++ VideoDigital.java
@@ -1,12 +1,16 @@
+// Criando classe ebook que extende (no caso é filha de) ItemBiblioteca... e implementa as interfaces Baixavel e Visualizavel
 public class VideoDigital extends ItemBibliotecaDigital implements Baixavel, Visualizavel {
 
+    // Criando atributo
     int minutagem;
 
+    // Construtor
     public VideoDigital(String autor, String titulo, int minutagem) {
         super(autor, titulo);
         this.minutagem = minutagem;
     }
 
+    // Implementand o metodo descricao
     @Override
     public void descricao() {
         System.out.println("");
@@ -16,11 +20,13 @@ public class VideoDigital extends ItemBibliotecaDigital implements Baixavel, Vis
         System.out.println("Por: " + autor);
     }
 
+    // Implementando metodo baixar
     public void baixar() {
         System.out.println("");
         System.out.println("Video '" + titulo + "' baixado!");
     }
 
+    // Implementando metodo visualizar
     @Override
     public void visualizar() {
         System.out.println("");
